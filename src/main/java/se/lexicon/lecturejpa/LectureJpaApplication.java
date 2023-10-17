1aa83087ead65d05ced9324e6163e3d3ab676030
Liebniz <anders.leif.loren@liebniz.com>
2023-10-17 09:35:36 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/lecturejpa/LectureJpaApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.lecturejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureJpaApplication.class, args);
+	}
+
+}
